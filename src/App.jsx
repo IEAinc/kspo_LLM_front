c890a60f80a2a27c33622d7cbef43e4fba855144
김상혁 <shkim@iea.co.kr>
2025-11-13 16:44:21 +0900

    기본세팅

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import Main from "./components/pages/Main.jsx";
+
+const App = () => {
+
+  return (
+    <>
+      <Router>
+        <Routes>
+          {/* 기본 경로에서 다른 페이지로 리다이렉트 */}
+          <Route path="/" element={<Navigate to="/main" replace />} />
+
+          <Route path="/main" element={<Main />} />
+
+          {/*<Route path="*" element={<NotFound />} />*/}
+        </Routes>
+      </Router>
+    </>
+  )
+}
+
+export default App;
